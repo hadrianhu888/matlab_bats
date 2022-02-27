870ed19603689540e1c57a756902b52927955681
hhu1618 <hadrianhu.gm@gmail.com>
2022-02-27 17:01:37 -0500

    minor syntax changes

--- matlab_copy_license_file.bat
+++ matlab_copy_license_file.bat
@@ -1,11 +1,21 @@
-REM this batch file copies the matlab crack dll to the correct folder to simplify this process without error
-@echo on
+echo on
+set logger=matlab_log_update.txt 
+if exists %logger%
+(
+    call :log > %logger%
+)
+else 
+(
+    exit /b 1
+)
+:log
+echo this batch file copies the matlab crack dll to the correct folder to simplify this process without error
 set dll_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
 set matlab_plugin_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
 set matlab = D:\Program Files\Matlab\bin\matlab.exe
-REM copies the libmwlmgrimpl.dll to lmgrimpl directory in Matlab install root folder
+echo copies the libmwlmgrimpl.dll to lmgrimpl directory in Matlab install root folder
 copy /y %dll_dir% %matlab_plugin_dir%
-REM successfully copied
+echo successfully copied
 cd %matlab_plugin_dir%
 start %matlab%
-@echo off 
+echo off 
