01b0f364083b7aaa68f4a10acf7350eb6839fc7f
hhu1618 <hadrianhu.gm@gmail.com>
2022-02-27 16:36:46 -0500

    updated the syntax to reflect correct copy method

--- matlab_copy_license_file.bat
+++ matlab_copy_license_file.bat
@@ -1,10 +1,11 @@
 REM this batch file copies the matlab crack dll to the correct folder to simplify this process without error
 @echo on
+set dll_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
+set matlab_plugin_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
+set matlab = D:\Program Files\Matlab\bin\matlab.exe
 REM copies the libmwlmgrimpl.dll to lmgrimpl directory in Matlab install root folder
-xcopy "C:\Users\lordx\Desktop\libmwlmgrimpl.dll" "D:\Program Files\Matlab\bin\win64\matlab_startup_plugins\lmgrimpl"
+copy /y %dll_dir% %matlab_plugin_dir%
 REM successfully copied
-%SystemRoot%\explorer.exe "D:\Program Files\Matlab\bin\win64\matlab_startup_plugins\lmgrimpl"
-tasklist /V /FI "IMAGENAME eq explorer.exe" /FI "WINDOWTITLE eq C*"
-taskkill /FI "IMAGENAME eq explorer.exe" /FI "WINDOWTITLE eq C*"
-start "D:\Program Files\Matlab\bin\matlab.exe"
+cd %matlab_plugin_dir%
+start %matlab%
 @echo off 
