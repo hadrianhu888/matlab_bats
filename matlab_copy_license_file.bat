b31ed9cac3d8c58a72c73ae4b58b4b7725e9f764
hhu1618 <hadrianhu.gm@gmail.com>
2022-02-27 17:33:41 -0500

    got logging to work

--- matlab_copy_license_file.bat
+++ matlab_copy_license_file.bat
@@ -1,21 +1,22 @@
 echo on
-set logger=matlab_log_update.txt 
-if exists %logger%
+echo date_time = %date% %time%
+set filename=%date_time%logger.txt
+call :log > %filename%
+if exist %filename%
 (
-    call :log > %logger%
+    call :log >> %filename%
 )
 else 
 (
     exit /b 1
 )
-:log
+:log 
 echo this batch file copies the matlab crack dll to the correct folder to simplify this process without error
-set dll_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
-set matlab_plugin_dir = C:\Users\lordx\Desktop\libmwlmgrimpl.dll
+set dll_dir = .\*.dll
 set matlab = D:\Program Files\Matlab\bin\matlab.exe
 echo copies the libmwlmgrimpl.dll to lmgrimpl directory in Matlab install root folder
-copy /y %dll_dir% %matlab_plugin_dir%
+copy /y *.dll D:\Program Files\Matlab\bin\win64\matlab_startup_plugins\lmgrimpl
 echo successfully copied
-cd %matlab_plugin_dir%
-start %matlab%
+cd D:\Program Files\Matlab\bin\win64\matlab_startup_plugins\lmgrimpl
+start D:\Program Files\Matlab\bin\matlab.exe
 echo off 
